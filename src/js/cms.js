d2405aaae0ee12240293ef4fc1b793959b6d55b3
Jamie Magee <jamie.magee@gmail.com>
2018-08-03 22:48:09 +0200

    Little more collection config
    Former-commit-id: 99d6c8e939b3a61046dae932cb8f7abae19127a4

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,4 +1,18 @@
 import React from 'react';
 import CMS from 'netlify-cms';
 
+class ColorControl extends React.Component {
+  render() {
+    return (
+      <input
+        style={{ height: '80px' }}
+        type="color"
+        value={this.props.value}
+        onInput={e => this.props.onChange(e.target.value)}
+      />
+    );
+  }
+}
+
 CMS.registerPreviewStyle('/css/final.css');
+CMS.registerWidget('color', ColorControl);
