38244b52bc80f117db0d445e65edd2fcc2a9f499
Jamie Magee <jamie.magee@gmail.com>
2018-08-03 22:08:06 +0200

    Fix js imports

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,37 +1,40 @@
-import webpack from "webpack";
-import path from "path";
+import webpack from 'webpack';
+import path from 'path';
 
 export default {
   module: {
     loaders: [
       {
         test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file-loader?name=/[hash].[ext]"
+        loader: 'file-loader?name=/[hash].[ext]'
       },
       {
-        loader: "babel-loader",
+        loader: 'babel-loader',
         test: /\.js?$/,
         exclude: /node_modules/,
-        query: {cacheDirectory: true}
+        query: { cacheDirectory: true }
       }
     ]
   },
 
   plugins: [
     new webpack.ProvidePlugin({
-      "fetch": "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch"
+      fetch:
+        'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch'
     })
   ],
 
-  context: path.join(__dirname, "src"),
+  context: path.join(__dirname, 'src'),
   entry: {
-    app: ["./js/app"],
-    cms: ["./js/cms"]
+    cms: ['./js/cms'],
+    jquery: ['./js/jquery'],
+    scrollIt: ['./js/scrollIt'],
+    stickUp: ['./js/stickUp']
   },
   output: {
-    path: path.join(__dirname, "dist"),
-    publicPath: "/",
-    filename: "[name].js"
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].js'
   },
-  externals:  [/^vendor\/.+\.js$/]
+  externals: [/^vendor\/.+\.js$/]
 };
