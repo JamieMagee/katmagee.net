549bf72e44736e35af47c26bd38ef460d8f54af4
Jamie Magee <jamie.magee@gmail.com>
2018-08-06 21:21:18 +0200

    Optimize images
    Former-commit-id: 06b235d566b488ead3dba8c26dac909f41e3734a

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,22 @@
-import gulp from 'gulp';
-import cp from 'child_process';
-import gutil from 'gulp-util';
-import postcss from 'gulp-postcss';
-import cssImport from 'postcss-import';
-import cssnext from 'postcss-cssnext';
 import BrowserSync from 'browser-sync';
+import cache from 'gulp-cache';
+import cp from 'child_process';
+import cssImport from 'postcss-import';
+import cssnano from 'cssnano';
+import cssnext from 'postcss-cssnext';
+import gulp from 'gulp';
+import gutil from 'gulp-util';
+import imagemin from 'gulp-imagemin';
+import imageminGiflossy from 'imagemin-giflossy';
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminPngquant from 'imagemin-pngquant';
+import imageminZopfli from 'imagemin-zopfli';
+import inject from 'gulp-inject';
+import postcss from 'gulp-postcss';
+import svgmin from 'gulp-svgmin';
+import svgstore from 'gulp-svgstore';
 import webpack from 'webpack';
 import webpackConfig from './webpack.conf';
-import svgstore from 'gulp-svgstore';
-import svgmin from 'gulp-svgmin';
-import inject from 'gulp-inject';
-import cssnano from 'cssnano';
 
 const browserSync = BrowserSync.create();
 const hugoBin = `./bin/hugo.${
@@ -26,8 +32,14 @@ gulp.task('hugo', cb => buildSite(cb));
 gulp.task('hugo-preview', cb =>
   buildSite(cb, ['--buildDrafts', '--buildFuture'])
 );
-gulp.task('build', ['css', 'js', 'cms-assets', 'hugo']);
-gulp.task('build-preview', ['css', 'js', 'cms-assets', 'hugo-preview']);
+gulp.task('build', ['css', 'js', 'cms-assets', 'hugo', 'imagemin']);
+gulp.task('build-preview', [
+  'css',
+  'js',
+  'cms-assets',
+  'hugo-preview',
+  'imagemin'
+]);
 
 gulp.task('css', () =>
   gulp
@@ -86,6 +98,43 @@ gulp.task('server', ['hugo', 'css', 'cms-assets', 'js', 'svg'], () => {
   gulp.watch('./src/css/**/*.css', ['css']);
   gulp.watch('./site/static/img/icons-*.svg', ['svg']);
   gulp.watch('./site/**/*', ['hugo']);
+});
+
+gulp.task('imagemin', function() {
+  return gulp
+    .src(['site/static/img/*.{gif,png,jpg}'])
+    .pipe(
+      cache(
+        imagemin([
+          imageminPngquant({
+            speed: 1,
+            quality: 98
+          }),
+          imageminZopfli({
+            more: true
+          }),
+          imageminGiflossy({
+            optimizationLevel: 3,
+            optimize: 3,
+            lossy: 2
+          }),
+          imagemin.svgo({
+            plugins: [
+              {
+                removeViewBox: false
+              }
+            ]
+          }),
+          imagemin.jpegtran({
+            progressive: true
+          }),
+          imageminMozjpeg({
+            quality: 60
+          })
+        ])
+      )
+    )
+    .pipe(gulp.dest('dist/img'));
 });
 
 function buildSite(cb, options) {
