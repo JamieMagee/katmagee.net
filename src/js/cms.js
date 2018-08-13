246aa02b27305e9d83ae0dcb4699cf891889936e
Jamie Magee <jamie.magee@gmail.com>
2018-08-13 21:09:35 +0200

    Add simple preview for case studies
    Former-commit-id: 53509ab1899d05b65601a24b001930786407ae68

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,6 +1,8 @@
 import CMS from 'netlify-cms';
 import React from 'react';
 
+import CaseStudyPreview from './cms-preview-templates/case-studies';
+
 class ColorControl extends React.Component {
   render() {
     return (
@@ -15,4 +17,8 @@ class ColorControl extends React.Component {
 }
 
 CMS.registerPreviewStyle('/css/final.css');
+CMS.registerPreviewStyle(
+  'https://fonts.googleapis.com/css?family=Roboto:300,400,700'
+);
 CMS.registerWidget('color', ColorControl);
+CMS.registerPreviewTemplate('casestudies', CaseStudyPreview);
