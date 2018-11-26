4c89cd6ebb430e64fd2283657b4aaeb40b655ad2
Jamie Magee <jamie.magee@gmail.com>
2018-11-26 13:29:23 +0100

    Redirect automatically after login
    Former-commit-id: 21cd8681334d77a03cf8003166e89e178e18c0b0

--- site/static/js/app.js
+++ site/static/js/app.js
@@ -9,3 +9,13 @@ $(function() {
     easing: 'ease-in-out'
   });
 });
+
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on('init', user => {
+    if (!user) {
+      window.netlifyIdentity.on('login', () => {
+        document.location.href = '/admin/';
+      });
+    }
+  });
+}
